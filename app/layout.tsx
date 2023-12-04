6b5a5d2a407633c68fb5b91261c9e0b76832018e
Dinuda <ydinuda@gmail.com>
2023-12-04 18:40:52 +0530

    feat: add ui components and children mapping with global header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,49 @@
+// These styles apply to every route in the application
 import "app/globals.css"
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Suspense } from "react";
+import Header from "components/header/header";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const title = "Next.js Prisma Postgres Auth Starter";
+const description =
+  "This is a Next.js starter kit that uses Next-Auth for simple email + password login and a Postgres database to persist the data.";
+
+export const metadata: Metadata = {
+  title,
+  description,
+  twitter: {
+    card: "summary_large_image",
+    title,
+    description,
+  },
+  metadataBase: new URL("https://nextjs-postgres-auth.vercel.app"),
+  creator: "shadcn",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.variable}>
+        <Suspense fallback="Loading...">
+          <Header />
+        </Suspense>
+        {children}
+
+      </body>
     </html>
-  )
+  );
 }
---
+++ components/ui/nav/nav-elements.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { NavLink } from "interface/ui/NavLink";
+
+
+export function NavElements({ navigationLinks }: { navigationLinks: NavLink[] }) {
+  return (
+    <nav
+      className="flex items-center space-x-4 lg:space-x-6">
+        {navigationLinks.map((link: { key: string, value: string}) => (
+            <Link
+            key={link.key}
+            href={`/${link.value}`}
+            className="text-sm font-medium transition-colors hover:text-primary"
+            >
+            {link.key}
+            </Link>
+        ))}
+    </nav>
+  );
+}
