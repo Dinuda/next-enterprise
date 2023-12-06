4aa437cf966768ff34c2f91400baf4ce85d7ba48
Dinuda <ydinuda@gmail.com>
2023-12-06 16:04:21 +0530

    feat: custom phone picker and validator

--- app/register/register-form.tsx
+++ app/register/register-form.tsx
@@ -1,10 +1,12 @@
 "use client"
-
+import "react-international-phone/style.css"
+import "./register-form.css"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
 import React from "react"
 import { useForm } from "react-hook-form"
+import { PhoneInput } from "react-international-phone"
 import * as z from "zod"
 
 import { Button } from "components/ui/button"
@@ -22,7 +24,7 @@ const RegisterFormSchema = z.object({
   doB: z.string().refine((val) => {
     if (!val) return false
     const date = new Date(val)
-    if(date > new Date() || date < new Date(1900, 1, 1)) return false
+    if (date > new Date() || date < new Date(1900, 1, 1)) return false
     return !isNaN(date.getTime())
   }),
 })
@@ -35,7 +37,7 @@ const defaultValues: Partial<RegisterFormValues> = {
   phone: "",
   studentName: "",
   address: "",
-  doB: ""
+  doB: "",
 }
 
 export function RegisterForm() {
@@ -70,103 +72,116 @@ export function RegisterForm() {
     })
   }
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="example@xyz.com" {...field} required />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="phone"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Phone</FormLabel>
-              <FormControl>
-                <Input placeholder="123-456-7890" {...field} required/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="studentName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Child name</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} required/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="address"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Address</FormLabel>
-              <FormControl>
-                <Input placeholder="1234 Main St" {...field} required/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="doB"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Date of birth</FormLabel>
-              <br></br>
-              <FormControl>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {field.value ? format(new Date(field.value), "PPP") : <span>Pick a date</span>}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0">
-                    <Calendar
-                      mode="single"
-                      selected={field.value ? new Date(field.value) : undefined}
-                      onSelect={(date) => {
-                        field.onChange(date!.toISOString()) // Assuming you want to store the date as ISO string
-                      }}
-                      initialFocus
-                      required/>
-                  </PopoverContent>
-                </Popover>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+  const handlePhoneChange = (phone: string) => {
+    form.setValue("phone", phone)
+  }
 
-        <Button type="submit" className="w-full" disabled={!form.formState.isValid}>
-          Sign up
-        </Button>
-      </form>
-    </Form>
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="example@xyz.com" {...field} required />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone</FormLabel>
+                <FormControl>
+                  <PhoneInput
+                    disableFormatting={true}
+                    defaultCountry="ua"
+                    {...field}
+                    onChange={handlePhoneChange}
+                    required
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="studentName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Child name</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} required />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Input placeholder="1234 Main St" {...field} required />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="doB"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Date of birth</FormLabel>
+                <br></br>
+                <FormControl>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[280px] justify-start text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {field.value ? format(new Date(field.value), "PPP") : <span>Pick a date</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={field.value ? new Date(field.value) : undefined}
+                        onSelect={(date) => {
+                          field.onChange(date!.toISOString()) // Assuming you want to store the date as ISO string
+                        }}
+                        initialFocus
+                        required
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full" disabled={!form.formState.isValid}>
+            Sign up
+          </Button>
+        </form>
+      </Form>
+    </>
   )
 }
